b7787858d87f7762425ac35ab99d27cbf9ba63b6
CrisGO <cristhiangiraldo053@gmail.com>
2025-05-17 16:33:21 -0500

    Correcciones a la nueva update de mason

--- .config/nvim/lua/plugins/languages/mason.lua
+++ .config/nvim/lua/plugins/languages/mason.lua
@@ -1,18 +1,22 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = { "williamboman/mason-lspconfig.nvim", "neovim/nvim-lspconfig" },
-	build = ":MasonUpdate",
-	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = { "lua_ls", "rust_analyzer", "jdtls" },
-			automatic_installation = true,
-		})
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  build = ":MasonUpdate",
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = { "lua_ls", "rust_analyzer", "jdtls" },
+      automatic_installation = true,
+    })
 
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				require("lspconfig")[server_name].setup({})
-			end,
-		})
-	end,
+    -- Configuración directa de LSP
+    local lspconfig = require("lspconfig")
+    local servers = { "lua_ls", "rust_analyzer", "jdtls" }
+    for _, server in ipairs(servers) do
+      lspconfig[server].setup({})
+    end
+  end,
 }
--- .config/nvim/lua/plugins/languages/typescript.lua
+++ .config/nvim/lua/plugins/languages/typescript.lua
@@ -1,18 +1,20 @@
 return {
 	"williamboman/mason.nvim",
-	dependencies = { "williamboman/mason-lspconfig.nvim", "neovim/nvim-lspconfig" },
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+	},
 	build = ":MasonUpdate",
 	config = function()
 		require("mason").setup()
 		require("mason-lspconfig").setup({
-			ensure_installed = { "ts_ls", "angularls", "eslint", "jsonls" },
+			ensure_installed = {
+				"ts_ls",
+				"angularls",
+				"eslint",
+				"jsonls",
+			},
 			automatic_installation = true,
-		})
-
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				require("lspconfig")[server_name].setup({})
-			end,
 		})
 	end,
 }
