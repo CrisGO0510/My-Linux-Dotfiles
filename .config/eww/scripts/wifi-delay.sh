4c0a2ad7bfae92f0e0f4358f68304cb25043b828
CrisGO <cristhiangiraldo053@gmail.com>
2025-08-08 22:46:18 -0500

    Correcciones de estilos

--- .config/eww/scripts/wifi-delay.sh
+++ .config/eww/scripts/wifi-delay.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 /usr/bin/eww update wifihov=true
-(sleep 0.45 && /usr/bin/eww update wifirev="$(/usr/bin/eww get wifihov)") &
+(sleep 0.45 && \
+  /usr/bin/eww update wifirev=$([ "$(/usr/bin/eww get wifihov)" = "true" ] && echo true || echo false)
+) &
