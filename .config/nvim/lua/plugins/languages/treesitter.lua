11e7c22ac1ec00e0ad9acfdd7e20ddccbaeaf0b4
CrisGO <cristhiangiraldo053@gmail.com>
2025-09-20 19:40:27 -0500

    Mejoras al treesitter

--- .config/nvim/lua/plugins/languages/treesitter.lua
+++ .config/nvim/lua/plugins/languages/treesitter.lua
@@ -1,40 +1,65 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-    },
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = { "lua", "typescript", "rust", "python", "json" },
-            highlight = { enable = true },
-            indent = { enable = true },
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				-- Angular + web
+				"angular",
+				"html",
+				"css",
+				"scss",
+				-- TS/JS base
+				"typescript",
+				"javascript",
+				"json",
+				"jsdoc",
+				"regex",
+			},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
 
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner",
-                    },
-                },
-                move = {
-                    enable = true,
-                    set_jumps = true,
-                    goto_next_start = {
-                        ["]m"] = "@function.outer",
-                        ["]c"] = "@class.outer",
-                    },
-                    goto_previous_start = {
-                        ["[m"] = "@function.outer",
-                        ["[c"] = "@class.outer",
-                    },
-                },
-            },
-        })
-    end,
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
+						["ab"] = "@block.outer",
+						["ib"] = "@block.inner",
+						["al"] = "@loop.outer",
+						["il"] = "@loop.inner",
+						["ai"] = "@conditional.outer",
+						["ii"] = "@conditional.inner",
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]m"] = "@function.outer",
+						["]c"] = "@class.outer",
+						["]p"] = "@parameter.outer",
+					},
+					goto_previous_start = {
+						["[m"] = "@function.outer",
+						["[c"] = "@class.outer",
+						["[p"] = "@parameter.outer",
+					},
+				},
+			},
+		})
+	end,
 }
