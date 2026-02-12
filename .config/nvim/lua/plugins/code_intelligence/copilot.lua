2a050b1e9ba340cfbc4f039cc71ce8264d72224a
CrisGO <cristhiangiraldo053@gmail.com>
2026-02-12 18:19:27 -0500

    Keymaps corregidos

--- .config/nvim/lua/plugins/code_intelligence/copilot.lua
+++ .config/nvim/lua/plugins/code_intelligence/copilot.lua
@@ -7,6 +7,14 @@ return {
 				suggestion = {
 					enabled = true,
 					auto_trigger = true,
+					keymap = {
+						accept = false,      -- Deshabilitar - manejado en keymaps.lua
+						accept_word = false, -- Deshabilitar - manejado en keymaps.lua  
+						accept_line = false, -- Deshabilitar - manejado en keymaps.lua
+						next = false,        -- Deshabilitar - manejado en keymaps.lua
+						prev = false,        -- Deshabilitar - manejado en keymaps.lua
+						dismiss = false,     -- Deshabilitar - manejado en keymaps.lua
+					},
 				},
 				panel = {
 					enabled = true,
