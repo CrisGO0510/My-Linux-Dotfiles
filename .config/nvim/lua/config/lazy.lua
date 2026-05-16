c639efb6100a7ab5ed2ae3c140a07b7b5a8a3397
CrisGO <cristhiangiraldo053@gmail.com>
2026-05-16 16:26:05 -0500

    mejoras de nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -24,8 +24,8 @@ require("lazy").setup({
 		-- ══════════════════════════════════════════════════════
 		-- Stacks de desarrollo: comentar/descomentar según la necesidad
 		-- ══════════════════════════════════════════════════════
-		{ import = "plugins.stacks.angular" },
-		-- { import = "plugins.stacks.vue" },
+		-- { import = "plugins.stacks.angular" },
+		{ import = "plugins.stacks.vue" },
 		-- { import = "plugins.stacks.typescript" },
 		-- { import = "plugins.stacks.csharp" },
 	},
--- .config/nvim/lua/plugins/languages/mason.lua
+++ .config/nvim/lua/plugins/languages/mason.lua
@@ -7,18 +7,6 @@ return {
 	build = ":MasonUpdate",
 	config = function()
 		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"ts_ls", -- TypeScript Language Server
-				"vue_ls", -- Volar (Vue Language Server oficial) 
-				"html", -- HTML Language Server (para templates separados)
-				"angularls", -- Angular Language Server
-				"eslint", -- ESLint LSP
-				"jsonls", -- JSON Language Server
-			},
-			automatic_installation = true,
-		})
 
 		-- Basic LSP capabilities (for completion support)
 		local capabilities = require('cmp_nvim_lsp').default_capabilities()
@@ -54,5 +42,96 @@ return {
 				},
 			},
 		}
+
+		-- Volar 2+/3 Hybrid Mode: ts_ls atiende .vue cargando @vue/typescript-plugin
+		local vue_ts_plugin = vim.fn.stdpath("data")
+			.. "/mason/packages/vue-language-server/node_modules/@vue/typescript-plugin"
+
+		-- Forzar capabilities estáticas para que ts_ls atienda .vue: si dejamos
+		-- dynamicRegistration en true, el server registra selectors que excluyen
+		-- "vue" y supports_method() devuelve false aunque definitionProvider sea true.
+		local ts_capabilities = vim.tbl_deep_extend("force", capabilities, {
+			textDocument = {
+				definition = { dynamicRegistration = false },
+				references = { dynamicRegistration = false },
+				hover = { dynamicRegistration = false },
+				implementation = { dynamicRegistration = false },
+				typeDefinition = { dynamicRegistration = false },
+				declaration = { dynamicRegistration = false },
+				completion = { dynamicRegistration = false },
+				rename = { dynamicRegistration = false },
+				signatureHelp = { dynamicRegistration = false },
+			},
+		})
+
+		vim.lsp.config.ts_ls = {
+			cmd = { "typescript-language-server", "--stdio" },
+			filetypes = {
+				"javascript",
+				"javascriptreact",
+				"javascript.jsx",
+				"typescript",
+				"typescriptreact",
+				"typescript.tsx",
+				"vue",
+			},
+			root_markers = { "tsconfig.json", "package.json", "jsconfig.json", ".git" },
+			capabilities = ts_capabilities,
+			init_options = {
+				plugins = {
+					{
+						name = "@vue/typescript-plugin",
+						location = vue_ts_plugin,
+						languages = { "vue" },
+					},
+				},
+			},
+			on_attach = function(client, bufnr)
+				on_attach(client, bufnr)
+				if client.name == "ts_ls" then
+					client.server_capabilities.documentFormattingProvider = false
+					client.server_capabilities.documentRangeFormattingProvider = false
+				end
+			end,
+			settings = {
+				typescript = {
+					inlayHints = {
+						includeInlayParameterNameHints = "none",
+						includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+						includeInlayFunctionParameterTypeHints = false,
+						includeInlayVariableTypeHints = false,
+						includeInlayPropertyDeclarationTypeHints = false,
+						includeInlayFunctionLikeReturnTypeHints = false,
+						includeInlayEnumMemberValueHints = false,
+					},
+				},
+				javascript = {
+					inlayHints = {
+						includeInlayParameterNameHints = "none",
+						includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+						includeInlayFunctionParameterTypeHints = false,
+						includeInlayVariableTypeHints = false,
+						includeInlayPropertyDeclarationTypeHints = false,
+						includeInlayFunctionLikeReturnTypeHints = false,
+						includeInlayEnumMemberValueHints = false,
+					},
+				},
+			},
+		}
+
+		-- Setup mason-lspconfig DESPUÉS de definir vim.lsp.config.* para que el
+		-- auto-enable use nuestros configs en vez de los defaults de nvim-lspconfig.
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"ts_ls", -- TypeScript Language Server
+				"vue_ls", -- Volar (Vue Language Server oficial)
+				"html", -- HTML Language Server (para templates separados)
+				"angularls", -- Angular Language Server
+				"eslint", -- ESLint LSP
+				"jsonls", -- JSON Language Server
+			},
+			automatic_installation = true,
+		})
 	end,
 }
