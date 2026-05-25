fff6136d20dc5e92b885f34b200043d567ea6e4f
CrisGO <cristhiangiraldo053@gmail.com>
2026-05-25 14:15:26 -0500

    Ajustes a resolución

--- .config/eww/scripts/monitor-detect.sh
+++ .config/eww/scripts/monitor-detect.sh
@@ -14,12 +14,12 @@ y_pos=$(echo "$cursor_pos" | cut -d',' -f2 | tr -d ' ')
 monitors_info=$(hyprctl monitors -j)
 
 # Determinar en qué monitor está el cursor
-monitor_id=0
+monitor_id=1
 
 # Monitor 0 (DP-3): 0-1919 en X
 # Monitor 1 (HDMI-A-1): 1920+ en X
-if [ "$x_pos" -ge 1920 ]; then
-    monitor_id=1
+if [ "$y_pos" -gt 1080 ]; then
+    monitor_id=0
 fi
 
 echo "$monitor_id"
